222389ffa39a170295a0638e59919ce77d28889c
AimJim <92112507+AimJim@users.noreply.github.com>
2022-11-28 11:37:03 +0100

    :(

--- empresaCoche/appEmpresaCoche/models.py
+++ empresaCoche/appEmpresaCoche/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.nombre
+
 class Car(models.Model):
     nombre= models.CharField(max_length=100)
     km=models.IntegerField()
@@ -6,6 +17,7 @@ class Car(models.Model):
     potencia=models.IntegerField()
     img_url=models.CharField(max_length=512)
     precio=models.IntegerField()
+    
 
     def __str__(self) -> str:
         return self.nombre
@@ -13,5 +25,7 @@ class Location(models.Model):
     nombre= models.CharField(max_length=50)
     def __str__(self) -> str:
         return self.nombre
+
+
 
 # Create your models here.
