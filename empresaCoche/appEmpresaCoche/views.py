5690d0e49019d233d2459b4800a090b17d4b738a
AimJim <92112507+AimJim@users.noreply.github.com>
2022-12-05 16:30:07 +0100

    Gauzatxoak

--- empresaCoche/appEmpresaCoche/views.py
+++ empresaCoche/appEmpresaCoche/views.py
@@ -34,12 +34,23 @@ def about(request):
     return render(request, "about.html", context)
 
 def booking(request):
+
+    if(request.method == "POST"):
+        carSearch = request.POST['carSelect']
+        print(carSearch)
+    else:
+        carSearch = '1'
+    try:
+        carSearch = int(carSearch)
+    except:
+        carSearch = 1
+    
     localizaciones = get_list_or_404(Location.objects.order_by('nombre'))
     coches = get_list_or_404(Car.objects.order_by('nombre'))
     transmission = get_list_or_404(Transmission.objects.all())
     marcas = get_list_or_404(Marca.objects.all())
     
-    context = {'localizaciones': localizaciones, 'coches': coches, 'transmision':transmission, 'marcas': marcas}
+    context = {'localizaciones': localizaciones, 'coches': coches, 'transmision':transmission, 'marcas': marcas , 'selectedCar':carSearch}
     return render(request, "booking.html", context)
 
 def contact(request):
